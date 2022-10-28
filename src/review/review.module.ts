bc7dd8f60cabd0e82d3ea4f87633bc87dbfcae42
NursSSS <nurik1kun@gmail.com>
2022-10-29 01:38:47 +0600

    fixed place module error

--- src/review/review.module.ts
+++ src/review/review.module.ts
@@ -1,12 +1,13 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from 'src/auth/auth.module';
 import { PlaceController } from 'src/place/place.controller';
 import { UsersModule } from 'src/users/users.module';
 import { ReviewEntity } from './entities';
 import { ReviewService } from './review.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([ReviewEntity]), UsersModule],
+  imports: [TypeOrmModule.forFeature([ReviewEntity]), UsersModule, forwardRef(() => AuthModule)],
   controllers: [PlaceController],
   providers: [ReviewService]
 })
