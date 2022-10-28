91d873dbf54097b545893cac0553066ca13b3245
NursSSS <nurik1kun@gmail.com>
2022-10-29 01:33:35 +0600

    fixed place module error

--- src/place/place.module.ts
+++ src/place/place.module.ts
@@ -1,12 +1,14 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from 'src/auth/auth.module';
 import { UsersModule } from 'src/users/users.module';
 import { PlaceEnEntity, PlaceEntity,  } from './entity';
 import { PlaceController } from './place.controller';
 import { PlaceService } from './place.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([PlaceEntity, PlaceEnEntity]), UsersModule],
+  imports: [TypeOrmModule.forFeature([PlaceEntity, PlaceEnEntity]), UsersModule, 
+  forwardRef(() => AuthModule)],
   controllers: [PlaceController],
   providers: [PlaceService]
 })
